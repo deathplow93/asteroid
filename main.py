1d495d1c4fd5f9458983d5267335e368d061e59e
deathplow93 <rafaelalvarez93@gmail.com>
2024-09-11 21:02:29 -0500

    added rotation

--- main.py
+++ main.py
@@ -19,11 +19,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     while running:
+        dt = clock.tick(60) / 1000  # divides by 1000 to convert milliseconds to seconds
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill("purple")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         clock.tick(60)
 
