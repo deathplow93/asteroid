0d1cd23226cf3357bb5272608c38a19b94d51adb
deathplow93 <rafaelalvarez93@gmail.com>
2024-11-29 22:33:48 -0600

    updated gameplay loop with groups

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+
     keep_game_running = True
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -21,10 +27,14 @@ def main():
             if event.type == pygame.QUIT:
                 keep_game_running = False
                 return print("Game has Quit")
+        for obj in updatable:
+            obj.update(dt)
 
-        player.update(dt)
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
